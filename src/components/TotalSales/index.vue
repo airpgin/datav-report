8101a67e0c00f195bd550a8b887220dbbda722e4
Asher <airpgin@gmail.com>
2021-09-11 23:17:25 +0800

    common card component

---
+++ src/components/TotalSales/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <common-card title="累计销售额" value="¥ 32,039,111">
+    <template>
+      <div class="compare-wrapper">
+        <div class="compare">
+          <span>日同比</span>
+          <span class="emphasis">7.33%</span>
+          <div class="increase" />
+        </div>
+        <div class="compare">
+          <span>月同比</span>
+          <span class="emphasis">3.33%</span>
+          <div class="decrease" />
+        </div>
+      </div>
+    </template>
+    <template #footer>
+      <span>昨日销售额 </span>
+      <span class="emphasis">¥ 23,232,442</span>
+    </template>
+  </common-card>
+</template>
+
+<script>
+import commonCardMixin from '@/mixins/commonCardMixin'
+
+export default {
+  mixins: [commonCardMixin]
+}
+</script>
+
+<style lang="scss" scoped>
+.compare-wrapper {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  .compare {
+    font-size: 12px;
+    margin-top: 3px;
+    color: #666;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <top-view />
+    <sales-view />
+    <buttom-view />
+    <map-view />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import TopView from '@/components/TopView'
+import SalesView from '@/components/SalesView'
+import ButtomView from '@/components/ButtomView'
+import MapView from '@/components/MapView'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    TopView,
+    SalesView,
+    ButtomView,
+    MapView
   }
 }
 </script>
+
+<style lang="scss">
+  .home {
+    border: 3px solid #cc0;
+    width: 100%;
+    height: 100%;
+    padding: 20px;
+    background-color: #eee;
+    box-sizing: border-box;
+  }
+</style>
